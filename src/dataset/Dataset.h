5c1b6b49df51fc112e598c6bbf60047b8582d36b
Louis Faury <l.faury@hotmail.fr>
2017-07-09 17:25:07 +0200

    Adds fct prototypes

--- src/dataset/Dataset.h
+++ src/dataset/Dataset.h
@@ -1,7 +1,11 @@
 /* file : Dataset.h
  * author : Louis Faury
  * date : 09/07/17
+ * brief : Simple dataset class for data storage (NUMERICAL ATTRIBUTES ONLY !)
  */
+
+#include "Eigen/Core"
+#include "string"
 
 #ifndef DATASET_H
 #define DATASET_H
@@ -10,6 +14,22 @@ class Dataset
 {
 public:
     Dataset();
+
+    void load(std::string fileName); // TODO
+    Eigen::MatrixXd sample(int sampleSize); // TODO
+    Eigen::MatrixXd sampleAndReplace(int sampleSize); // TODO
+
+
+    Eigen::MatrixXd getInputs(){ return m_inputs; }
+    Eigen::MatrixXd getOutputs(){ return m_outputs; }
+    int getInputSize(){ returm m_inputSize; }
+    int getOutputSize(){ returm m_outputSize; }
+
+protected:
+    Eigen::MatrixXd m_inputs;
+    Eigen::MatrixXd m_outputs;
+    int m_inputSize;
+    int m_outputSize;
 };
 
 #endif // DATASET_H
