b4817d5905789108fad63bc1235dde89a5695212
louisfaury <l.faury@hotmail.fr>
2017-08-12 15:35:20 +0200

    Adds lc script and img

---
+++ scripts/learningcurves.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+""" Header """
+import numpy as np 
+import matplotlib.pyplot as plt
+
+""" Displaying learning curves """
+# Importing data 
+lglogfile = "../log/learninglogs.txt"
+lgdata = np.genfromtxt(lglogfile,delimiter=',')
+# Display 
+iters = np.arange(0,(np.shape(lgdata))[0])
+trainLoss = lgdata[:,0]
+testLoss = lgdata[:,1]
+
+f,(ax1,ax2) = plt.subplots(2,sharex=True)
+ax1.plot(iters,trainLoss)
+ax1.set_title('Training data loss')
+ax2.plot(iters,testLoss,'r')
+ax2.set_title('Testing data loss')
+plt.savefig("../img/classic_learningcurves")
